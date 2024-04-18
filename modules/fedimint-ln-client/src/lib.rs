408ac5610313a50ad4cf35d2446a06183fba3d70
joschisan <122358257+joschisan@users.noreply.github.com>
2024-04-18 13:14:35 +0200

    chore: track amount in ClientOutput

--- modules/fedimint-ln-client/src/lib.rs
+++ modules/fedimint-ln-client/src/lib.rs
@@ -645,6 +645,7 @@ impl LightningClientModule {
         Ok((
             ClientOutput {
                 output: ln_output,
+                amount: contract_amount,
                 state_machines: sm_gen,
             },
             contract_id,
@@ -671,7 +672,7 @@ impl LightningClientModule {
                 })?,
         };
 
-        let (incoming_output, contract_id) = create_incoming_contract_output(
+        let (incoming_output, amount, contract_id) = create_incoming_contract_output(
             &self.module_api,
             *payment_hash,
             invoice_amount,
@@ -681,6 +682,7 @@ impl LightningClientModule {
 
         let client_output = ClientOutput::<LightningOutputV0, LightningClientStateMachines> {
             output: incoming_output,
+            amount,
             state_machines: Arc::new(move |txid, _| {
                 vec![LightningClientStateMachines::InternalPay(
                     IncomingStateMachine {
@@ -890,6 +892,7 @@ impl LightningClientModule {
             invoice,
             ClientOutput {
                 output: ln_output,
+                amount: Amount::ZERO,
                 state_machines: sm_gen,
             },
             preimage.into_32(),
@@ -1102,6 +1105,7 @@ impl LightningClientModule {
 
         let output = self.client_ctx.make_client_output(ClientOutput {
             output: LightningOutput::V0(client_output.output),
+            amount: client_output.amount,
             state_machines: client_output.state_machines,
         });
 
@@ -1792,7 +1796,7 @@ pub async fn create_incoming_contract_output(
     payment_hash: sha256::Hash,
     amount_msat: Amount,
     redeem_key: secp256k1::KeyPair,
-) -> Result<(LightningOutputV0, ContractId), IncomingSmError> {
+) -> Result<(LightningOutputV0, Amount, ContractId), IncomingSmError> {
     let offer = fetch_and_validate_offer(module_api, payment_hash, amount_msat).await?;
     let our_pub_key = secp256k1::PublicKey::from_keypair(&redeem_key);
     let contract = IncomingContract {
@@ -1807,7 +1811,7 @@ pub async fn create_incoming_contract_output(
         contract: Contract::Incoming(contract),
     });
 
-    Ok((incoming_output, contract_id))
+    Ok((incoming_output, offer.amount, contract_id))
 }
 
 #[derive(Debug, Encodable, Decodable, Serialize)]
